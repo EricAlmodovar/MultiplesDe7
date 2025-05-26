28c0e1c659f623c49190d54d3644a7d3d4e29149
EricAlmodovar <ealmodovar.dam@institutcampalans.net>
2025-05-26 20:54:45 +0200

    1r Commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Introdueix un valor límit: ");
+        string? input = Console.ReadLine();
+        int limit = 0;
+
+        if (!int.TryParse(input, out limit))
+        {
+            Console.WriteLine("Valor no vàlid.");
+            return;
+        }
+
+        List<int> multiples = Metodes.comptadorMultiples(limit);
+
+        Console.WriteLine("Els múltiples de 7 més petits que " + limit + " són:");
+        Console.WriteLine(string.Join(", ", multiples));
+        Console.WriteLine("Hi han " + multiples.Count + " nombres múltiples de 7 més petits que " + limit + "."); // hola
+    }
+}
+
+internal class Metodes
+{
+    public static List<int> comptadorMultiples(int v)
+    {
+        List<int> multiples = new List<int>();
+
+        for (int i = 1; i < v; i++)
+        {
+            if (i % 7 == 0)
+            {
+                multiples.Add(i);
+            }
+        }
+
+        return multiples;
+    }
+}
